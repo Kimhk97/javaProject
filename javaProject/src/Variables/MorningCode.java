6fef016f14e268ee428bfd313d7ee5594ffd967f
admin <admin@YD01-20>
2021-02-19 12:36:39 +0900

    210219 자바연습

---
+++ javaProject/src/Variables/MorningCode.java
@@ -0,0 +1,20 @@
+package Variables;
+
+public class MorningCode {
+	public static void main(String[] args) {
+		int n1 = 30;
+		int n2 = 25; //내 나이
+		if(n1 < n2) {
+			System.out.println("나이가 좀 들었습니다.");
+		}
+		else if(n1 == n2) {
+			System.out.println("적정한 나이입니다.");
+		}
+		else {
+			System.out.println("한창이네요.");
+		
+		}
+	}
+	
+
+}
