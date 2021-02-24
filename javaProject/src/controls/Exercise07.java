768fa50bd19ab437626a7553c1e400f0b5a60e49
admin <admin@YD01-20>
2021-02-24 12:48:15 +0900

    -

--- javaProject/src/controls/Exercise07.java
+++ javaProject/src/controls/Exercise07.java
@@ -6,6 +6,14 @@ public class Exercise07 { //책 그대로 입력
 		int balance = 0;
 		scn.nextInt(); //숫자값을 읽어온다
 		Scanner scanner = new Scanner(system.in);
+		
+		while(run) {
+			System.out.println("--------------------------------");
+			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+			System.out.println("--------------------------------");
+			System.out.println("선택> ");
+		}		
+		System.out.println("프로그램 종료");
 	}
 	
 
