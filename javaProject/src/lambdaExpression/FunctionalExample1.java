2d4c17aabd4e04ec5b5112498f4c3e5361d0c2b2
admin <admin@YD01-20>
2021-03-15 17:41:31 +0900

    어영부영 0315

--- javaProject/src/lambdaExpression/FunctionalExample1.java
+++ javaProject/src/lambdaExpression/FunctionalExample1.java
@@ -9,16 +9,15 @@ public class FunctionalExample1 {
 	private static List<Student> list = Arrays.asList(new Student("홍길동", 90, 96), new Student("신용권", 95, 93));
 
 	public static void printString(Function<Student, String> function) {
-		for (Student student : list) {
-			System.out.println(function.apply(student) + " ");
+		for (Student student : list) { //list에 저장된 항목 수만큼 루핑
+			System.out.println(function.apply(student) + " "); //람다식 실행
 		}
 		System.out.println();
 	}
 
 	public static void printInt(ToIntFunction<Student> function) {
 		for (Student student : list) {
-			System.out.println(function.applyAsInt(student) + " ");
-
+			System.out.println(function.applyAsInt(student) + " "); //람다식 실행
 		}
 		System.out.println();
 	}
